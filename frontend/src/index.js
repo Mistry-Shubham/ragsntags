2a48467e0f77e5b1ee36864e0be08c74c4654787
Shubham-Mistry <mistryshubham29@gmail.com>
2022-01-08 22:22:37 +0530

    Step 2 Chakra UI Installation and Setup.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,10 @@
 import { render } from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
-render(<App />, document.querySelector('#root'));
+render(
+	<ChakraProvider>
+		<App />
+	</ChakraProvider>,
+	document.querySelector('#root')
+);
