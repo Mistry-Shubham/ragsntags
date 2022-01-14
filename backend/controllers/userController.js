b768007a65f92952c1f8c7c730407e06b1c6ecfb
Shubham-Mistry <mistryshubham29@gmail.com>
2022-01-14 11:59:00 +0530

    Step 57 Admin Middleware and Getting Users Endpoint.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -99,3 +99,17 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
 		throw new Error('User not found');
 	}
 });
+
+//@desc     Get all user details
+//@route    Get/api/users
+//@access   private/admin
+export const getUsers = asyncHandler(async (req, res) => {
+	const users = await User.find({});
+
+	if (users) {
+		res.status(200).json(users);
+	} else {
+		res.status(404);
+		throw new Error('Users Not Found');
+	}
+});
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -22,3 +22,12 @@ export const protect = asyncHandler(async (req, res, next) => {
 		throw new Error('Token not found');
 	}
 });
+
+export const admin = asyncHandler(async (req, res, next) => {
+	if (req.user && req.user.isAdmin) {
+		next();
+	} else {
+		res.status(401);
+		throw new Error('Not authorized, Only for Admins');
+	}
+});
