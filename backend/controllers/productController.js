d9a1bee73c64d699651098b53c10a5aebd1f7ec8
Shubham-Mistry <mistryshubham29@gmail.com>
2022-01-15 18:22:42 +0530

    Step 75 Adding product reviews on the frontend.
    Bug fix - In createproductreview controller in backend changed pust to push.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -118,7 +118,7 @@ export const createProductReview = asyncHandler(async (req, res) => {
 			user: req.user._id,
 		};
 
-		product.reviews.pust(review);
+		product.reviews.push(review);
 		product.numReviews = product.reviews.length;
 
 		product.rating =
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -10,12 +10,22 @@ import {
 	Button,
 	Icon,
 	Select,
+	FormControl,
+	FormLabel,
+	Textarea,
+	Box,
+	Link,
+	Spacer,
 } from '@chakra-ui/react';
 import { MdAddShoppingCart } from 'react-icons/md';
-import { listProductDetails } from '../actions/productActions';
+import {
+	listProductDetails,
+	createProductReview,
+} from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Rating from '../components/Rating';
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants';
 
 const ProductScreen = () => {
 	const dispatch = useDispatch();
@@ -23,16 +33,39 @@ const ProductScreen = () => {
 	const { id } = useParams();
 
 	const [qty, setQty] = useState(1);
+	const [rating, setRating] = useState(0);
+	const [comment, SetComment] = useState('');
 
 	const productDetails = useSelector((state) => state.productDetails);
 	const { loading, product, error } = productDetails;
 
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+
+	const productCreateReview = useSelector((state) => state.productCreateReview);
+	const {
+		loading: loadingCreateReview,
+		success: successCreateReview,
+		error: errorCreateReview,
+	} = productCreateReview;
+
 	useEffect(() => {
+		if (successCreateReview) {
+			alert('Review submitted');
+			setRating(0);
+			SetComment('');
+			dispatch({ type: PRODUCT_CREATE_REVIEW_RESET });
+		}
 		dispatch(listProductDetails(id));
-	}, [dispatch, id]);
+	}, [dispatch, id, successCreateReview]);
 
 	const addToCartHandler = () => {
 		navigate(`/cart/${id}?qty=${qty}`);
+	};
+
+	const reviewSubmitHandler = (e) => {
+		e.preventDefault();
+		dispatch(createProductReview(id, { rating, comment }));
 	};
 
 	return (
@@ -139,6 +172,111 @@ const ProductScreen = () => {
 					</Flex>
 				</Grid>
 			)}
+
+			<Box
+				paddingX="10"
+				paddingY="4"
+				borderRadius="lg"
+				backgroundColor="white"
+				marginTop="8"
+			>
+				<Heading as="h3" fontSize="2xl" marginBottom="2">
+					Reviews
+				</Heading>
+
+				{product.reviews.length === 0 && <Message>No Reviews</Message>}
+
+				{product.reviews.map((review, idx) => (
+					<Box
+						key={idx + 1}
+						paddingX="4"
+						paddingY="1"
+						backgroundColor="cyan.50"
+						borderRadius="xl"
+						boxShadow="xl"
+						marginBottom="4"
+					>
+						<Flex direction="column" padding="1">
+							<Flex alignItems="center" justifyContent="space-between">
+								<Text fontSize="lg" fontWeight="semibold">
+									{review.name}
+								</Text>
+								<Rating value={review.rating} color="yellow.500" />
+							</Flex>
+							{review.comment}
+						</Flex>
+					</Box>
+				))}
+			</Box>
+
+			<Box
+				paddingX="10"
+				paddingY="4"
+				borderRadius="lg"
+				backgroundColor="gray.100"
+				marginTop="8"
+			>
+				<Heading as="h3" fontSize="2xl" marginBottom="4">
+					Write a Review
+				</Heading>
+
+				{errorCreateReview && (
+					<Message type="error">{errorCreateReview}</Message>
+				)}
+
+				{userInfo ? (
+					<form onSubmit={reviewSubmitHandler}>
+						<FormControl id="rating">
+							<FormLabel>Rating</FormLabel>
+							<Select
+								placeholder="Select Rating"
+								value={rating}
+								onChange={(e) => setRating(e.target.value)}
+								borderColor="black"
+								borderRadius="xl"
+							>
+								<option value="1">1 - Worst</option>
+								<option value="2">2 - Poor</option>
+								<option value="3">3 - Moderate</option>
+								<option value="4">4 - Good</option>
+								<option value="5">5 - Best</option>
+							</Select>
+						</FormControl>
+
+						<Spacer height="3" />
+
+						<FormControl id="comment">
+							<FormLabel>Comment</FormLabel>
+							<Textarea
+								placeholder="Write your comment"
+								value={comment}
+								onChange={(e) => SetComment(e.target.value)}
+								borderColor="black"
+								borderRadius="xl"
+							/>
+						</FormControl>
+
+						<Spacer height="3" />
+
+						<Button
+							type="submit"
+							colorScheme="teal"
+							color="white"
+							_hover={{ color: 'cyan' }}
+						>
+							Post
+						</Button>
+					</form>
+				) : (
+					<Message>
+						Please{' '}
+						<Link as={RouterLink} to="/login">
+							Login
+						</Link>{' '}
+						to write a review
+					</Message>
+				)}
+			</Box>
 		</>
 	);
 };
