04b304c0f24fe2115f8d8ed41126a8a56466b414
Shubham Mistry <mistryshubham29@gmail.com>
2025-02-21 21:16:19 +0530

    change from cyclic to render

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,28 +1,28 @@
-import express from 'express';
+import express from "express";
 import {
-	authUser,
-	getUserProfile,
-	registerUser,
-	updateUserProfile,
-	getUsers,
-	deleteUser,
-	getUserById,
-	updateUser,
-} from '../controllers/userController.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router
-	.route('/:id/admin')
-	.get(protect, admin, getUserById)
-	.put(protect, admin, updateUser)
-	.delete(protect, admin, deleteUser);
-router.route('/login').post(authUser);
+  .route("/:id/admin")
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
+  .delete(protect, admin, deleteUser);
+router.route("/login").post(authUser);
 router
-	.route('/profile')
-	.get(protect, getUserProfile)
-	.put(protect, updateUserProfile);
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
